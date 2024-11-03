bc9e0ce86716e0ee3338b622c8e0ea6fda67d8dd
Durga Prasad Bokka <120996904+bokkadurgaprasad@users.noreply.github.com>
2024-11-03 09:20:57 +0530

    Add files via upload

---
+++ 07_JS_LEVEL_ONE/part7_for.js
@@ -0,0 +1,11 @@
+// For loop
+
+// for (var i = 0; i < 5; i++ ){
+//     console.log("Number is "+i)
+// }
+
+var word = "ABABABABABAB"
+
+for(var i = 1; i < word.length; i=i+2){
+    console.log(word[i])
+}
